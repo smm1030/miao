ac4207f845d90865d26ca603eb5b320c27025fca
smm1030 <smm1030@github.com>
2022-09-14 17:33:38 +0800

    chunk

--- lodash/smm1030-lodash.js
+++ lodash/smm1030-lodash.js
@@ -1,5 +1,24 @@
 var smm1030 = {
   chunk: function (array, size = 1) {
-    
+    var len = array.length
+    var result = []
+    var count = 0
+    for (var i = 0; i < len; i++) {
+      var arr = []
+      count++
+      if (count <= 3) {
+        arr.push(array[i])
+      }
+      if (count = 3) {
+        result.push(result)
+        count = 0
+      }
+    }
+    if (count < 3) {
+      result.push(arr)
+    }
+    return result
   }
+
+
 }
