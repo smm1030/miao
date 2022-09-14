f39171bb442cdbcbacabc6da22f18aba2bd2caae
smm1030 <smm1030@github.com>
2022-09-14 22:13:02 +0800

    compact

--- lodash/smm1030-lodash.js
+++ lodash/smm1030-lodash.js
@@ -19,7 +19,20 @@ var smm1030 = {
       result.push(arr)
     }
     return result
-  }
+  },
 
+  compact: function (array) {
+    var len = array.length
+    var result = []
+    for (var i = 0; i < len; i++) {
+      var a = array[i]
+      if (a !== false && a !== null && a !== 0 && a !== '' && a !== undefined && typeof (a) !== 'NaN') {
+        result.push(a)
+      }
+    }
+    return result
+  },
+
+  
 
 }
