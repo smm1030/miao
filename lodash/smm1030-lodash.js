10bcf6c915344107c5925a17a8ac38501379f7c2
smm1030 <smm1030@github.com>
2022-09-28 21:13:15 +0800

    sortedLastIndex

--- lodash/smm1030-lodash.js
+++ lodash/smm1030-lodash.js
@@ -340,7 +340,7 @@ var smm1030 = function() {
     var len = array.length
     for (var i = len - 1; i >= 0; i--) {
       if (array[i] == value) {
-        return i
+        return i + 1
       }
     }
   }
